011f7faeeb8219771c3008d4b85147c6f6b8a7a8
Valerio Terragni <v.terragni@auckland.ac.nz>
2023-04-28 13:47:25 +1200

    add test cases

--- src/test/java/nz/ac/auckland/se754/BankAccountTest.java
+++ src/test/java/nz/ac/auckland/se754/BankAccountTest.java
@@ -2,9 +2,33 @@ package nz.ac.auckland.se754;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class BankAccountTest {
 
+  private BankAccount bank;
+
+  @BeforeEach
+  public void setUp(){
+    bank = new BankAccount();
+  }
+
+  @Test
+  public void test1(){
+bank.deposit(10.00);
+double balance = bank.getBalance();
+assertEquals(10,balance);
+
+  }
+
+  @Test
+  public void test2(){
+    bank.deposit(100.00);
+    bank.withdraw(10.00);
+    double balance = bank.getBalance();
+    assertEquals(90,balance);
+
+  }
 
 }
